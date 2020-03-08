84f3f7b38f4faefd14e7bd57373cefe1d3fd5186
Tyler Croley <tjcroley@outlook.com>
2020-03-08 02:53:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,14 @@
 class Anagram
-  attr_accessor :words
+  attr_accessor :word
   
-  def initialize
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(word_array)
+    word_array.selct do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
     
 end
